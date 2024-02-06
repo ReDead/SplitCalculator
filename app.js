6c3c7cf5160e6b43a3ef6c29ba7cba1602a80635
7itanium <126641718+7itanium@users.noreply.github.com>
2024-02-06 13:16:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const split400Output = document.getElementById('split400')
 const milePaceOutput = document.getElementById('milePace')
+const targetOutput = document.getElementById('targetPace')
 const distanceInput = document.getElementById('distance')
 const timeInput = document.getElementById('time')
+const targetInput = document.getElementById('target')
 
 document.getElementById('button').onclick = function() {
    distance = parseInt(distanceInput.value)
    time = timeInput.value
+   target = parseInt(targetInput.value)
 
    if(isNaN(distance)) {
+      invalid()
+      return
+   }
+
+   if(isNaN(target)) {
       invalid()
       return
    }
@@ -52,11 +60,24 @@ document.getElementById('button').onclick = function() {
       mileTimeStr = Math.floor(mileTime / 60) + ':' + Math.round(mileTime % 60)
    }
 
+   target_time = total_seconds * (target / miles)
+   if(target_time>=100) {
+      if((target_time % 60).toFixed(2) < 10){
+         targetTimeStr = Math.floor(target_time / 60) + ':0' + (target_time % 60).toFixed(2)
+      } else {
+      targetTimeStr = Math.floor(target_time / 60) + ':' + (target_time % 60).toFixed(2)
+      }
+   } else {
+      targetTimeStr = target_time.toFixed(2)
+   }
+
    split400Output.innerHTML = lapTimeStr
    milePaceOutput.innerHTML = mileTimeStr
+   targetOutput.innerHTML = targetTimeStr
 };
 
 function invalid() {
    split400Output.innerHTML = 'Invalid input'
    milePaceOutput.innerHTML = 'Invalid input'
+   targetOutput.innerHTML = 'Invalid input'
 }
